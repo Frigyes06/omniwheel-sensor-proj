43b87a5a0dea4524f905b711182714657774cae9
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2020-10-19 16:29:41 +0200

    Updated distance_sensor.ino
    Added sum and serial monitoring.

--- distance_sensor/distance_sensor.ino
+++ distance_sensor/distance_sensor.ino
@@ -22,7 +22,8 @@ void setup() {
   pinMode(echoForward, INPUT);
   pinMode(echoLeft, INPUT);
   pinMode(echoBackward, INPUT);
-  pinMode(echoRight, INPUT); 
+  pinMode(echoRight, INPUT);
+  Serial.begin(9600);
 }
 
 void loop() {
@@ -32,24 +33,32 @@ void loop() {
    elore.tmp3=elore.tmp2;
    elore.tmp2=elore.tmp1;
    elore.tmp1=getDistance(4);
+   elore.atlag = (elore.tmp1 + elore.tmp2 + elore.tmp3 + elore.tmp4 + elore.tmp5)/5;
+   Serial.println(elore.atlag);
   //2 szenzor
    hatra.tmp5=hatra.tmp4;
    hatra.tmp4=hatra.tmp3;
    hatra.tmp3=hatra.tmp2;
    hatra.tmp2=hatra.tmp1;
    hatra.tmp1=getDistance(6);
+   hatra.atlag = (hatra.tmp1 + hatra.tmp2 + hatra.tmp3 + hatra.tmp4 + hatra.tmp5)/5;
+   Serial.println(hatra.atlag);
   //3 szenzor
    jobbra.tmp5=jobbra.tmp4;
    jobbra.tmp4=jobbra.tmp3;
    jobbra.tmp3=jobbra.tmp2;
    jobbra.tmp2=jobbra.tmp1;
    jobbra.tmp1=getDistance(7);
+   jobbra.atlag = (jobbra.tmp1 + jobbra.tmp2 + jobbra.tmp3 + jobbra.tmp4 + jobbra.tmp5)/5;
+   Serial.println(jobbra.atlag);
   //4 szenzor
    balra.tmp5=balra.tmp4;
    balra.tmp4=balra.tmp3;
    balra.tmp3=balra.tmp2;
    balra.tmp2=balra.tmp1;
    balra.tmp1=getDistance(5);
+   balra.atlag = (balra.tmp1 + balra.tmp2 + balra.tmp3 + balra.tmp4 + balra.tmp5)/5;
+   Serial.println(balra.atlag);
 }
 
 int getDistance(int pin){
