554794b1af5bab8e65aa6160d3ddc3013c7ee627
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2020-10-19 16:13:45 +0200

    distance_sensor.ino updated
    added struct, and 4 sensor support

--- distance_sensor/distance_sensor.ino
+++ distance_sensor/distance_sensor.ino
@@ -4,47 +4,59 @@
 #define echoBackward 6
 #define echoRight 7
 
+struct mozgo_atlag{
+  int tmp1=1, tmp2=1, tmp3=1, tmp4=1, tmp5=1;
+  double atlag;
+};
 
-int tmp1;
-int tmp2;
-int tmp3;
-int tmp4;
-int tmp5;
+int tmp1=1;
+int tmp2=1;
+int tmp3=1;
+int tmp4=1;
+int tmp5=1;
+
+mozgo_atlag elore, hatra, jobbra, balra;
 
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoForward, INPUT);
   pinMode(echoLeft, INPUT);
   pinMode(echoBackward, INPUT);
-  pinMode(echoRight, INPUT);
+  pinMode(echoRight, INPUT); 
 }
 
 void loop() {
-  int atlag;
-  delay(20);
-  tmp5=tmp4;
-  tmp4=tmp3;
-  tmp3=tmp2;
-  tmp2=tmp1;
-  tmp1=getDistance(4);
-  delay(20);
-  tmp2 = getDistance(4);
-  delay(20);
-  tmp3 = getDistance(4);
-  delay(20);
-  tmp4 = getDistance(4);
-  delay(20);
-  tmp5 = getDistance(4);
-    tmp1 = getDistance(4);
-
-  atlag = (tmp1 + tmp2 + tmp3 + tmp4 + tmp5)/5;
-  delay(20);
+  //1 szenzor
+   elore.tmp5=elore.tmp4;
+   elore.tmp4=elore.tmp3;
+   elore.tmp3=elore.tmp2;
+   elore.tmp2=elore.tmp1;
+   elore.tmp1=getDistance(4);
+  //2 szenzor
+   hatra.tmp5=hatra.tmp4;
+   hatra.tmp4=hatra.tmp3;
+   hatra.tmp3=hatra.tmp2;
+   hatra.tmp2=hatra.tmp1;
+   hatra.tmp1=getDistance(6);
+  //3 szenzor
+   jobbra.tmp5=jobbra.tmp4;
+   jobbra.tmp4=jobbra.tmp3;
+   jobbra.tmp3=jobbra.tmp2;
+   jobbra.tmp2=jobbra.tmp1;
+   jobbra.tmp1=getDistance(7);
+  //4 szenzor
+   balra.tmp5=balra.tmp4;
+   balra.tmp4=balra.tmp3;
+   balra.tmp3=balra.tmp2;
+   balra.tmp2=balra.tmp1;
+   balra.tmp1=getDistance(5);
 }
 
 int getDistance(int pin){
     long duration;
     int distance;
     digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
     digitalWrite(trigPin, LOW);
     duration = pulseIn(pin, HIGH);
     distance = duration * 0.034 / 2;
