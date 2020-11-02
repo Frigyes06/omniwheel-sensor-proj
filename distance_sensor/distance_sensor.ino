d444a0076f924968a5e07182fdd82b39f5da0a5e
Frigyes06 <72750891+Frigyes06@users.noreply.github.com>
2020-11-02 15:48:35 +0100

    Updated pins
    replaced pins, replaced numbers of pins with variables.

--- distance_sensor/distance_sensor.ino
+++ distance_sensor/distance_sensor.ino
@@ -1,8 +1,8 @@
-#define trigPin 3
-#define echoForward 4
-#define echoLeft 5
-#define echoBackward 6
-#define echoRight 7
+#define trigPin 10
+#define echoForward A0
+#define echoLeft A1
+#define echoBackward A2
+#define echoRight A3
 
 struct mozgo_atlag{
   int tmp1=1, tmp2=1, tmp3=1, tmp4=1, tmp5=1;
@@ -32,28 +32,28 @@ void loop() {
    elore.tmp4=elore.tmp3;
    elore.tmp3=elore.tmp2;
    elore.tmp2=elore.tmp1;
-   elore.tmp1=getDistance(4);
+   elore.tmp1=getDistance(echoForward);
    elore.atlag = (elore.tmp1 + elore.tmp2 + elore.tmp3 + elore.tmp4 + elore.tmp5)/5;
   //2 szenzor
    hatra.tmp5=hatra.tmp4;
    hatra.tmp4=hatra.tmp3;
    hatra.tmp3=hatra.tmp2;
    hatra.tmp2=hatra.tmp1;
-   hatra.tmp1=getDistance(6);
+   hatra.tmp1=getDistance(echoBackward);
    hatra.atlag = (hatra.tmp1 + hatra.tmp2 + hatra.tmp3 + hatra.tmp4 + hatra.tmp5)/5;
   //3 szenzor
    jobbra.tmp5=jobbra.tmp4;
    jobbra.tmp4=jobbra.tmp3;
    jobbra.tmp3=jobbra.tmp2;
    jobbra.tmp2=jobbra.tmp1;
-   jobbra.tmp1=getDistance(7);
+   jobbra.tmp1=getDistance(echoRight);
    jobbra.atlag = (jobbra.tmp1 + jobbra.tmp2 + jobbra.tmp3 + jobbra.tmp4 + jobbra.tmp5)/5;
   //4 szenzor
    balra.tmp5=balra.tmp4;
    balra.tmp4=balra.tmp3;
    balra.tmp3=balra.tmp2;
    balra.tmp2=balra.tmp1;
-   balra.tmp1=getDistance(5);
+   balra.tmp1=getDistance(echoLeft);
    balra.atlag = (balra.tmp1 + balra.tmp2 + balra.tmp3 + balra.tmp4 + balra.tmp5)/5;
    Serial.print(elore.atlag);
    Serial.print(hatra.atlag);
